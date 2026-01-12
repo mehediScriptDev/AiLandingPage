86afe793c1ff4b9c7af5b5a0a5b98538e653ffd3
Mehedi ScriptDev <mehedi25122003@gmail.com>
2026-01-12 22:56:08 +0600

    structure complete

--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,11 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
 import { RouterProvider } from "react-router/dom";
-import { router } from './router/routes.jsx';
-// import { AuthProvider } from './components/AuthProvider.jsx';
+import { router } from "./router/routes.jsx";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-   
-      {/* <AuthProvider> */}
-        <RouterProvider router={router} />
-        
-      {/* </AuthProvider> */}
-    
-  </StrictMode>,
-)
+    <RouterProvider router={router} />
+  </StrictMode>
+);
